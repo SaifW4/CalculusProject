d6695f462afed1dc8c86b6d1fcade29f1444de0d
SAif Walid <swa1150@rit.edu>
2022-04-05 10:29:00 -0700

    ca

--- Limits.py
+++ Limits.py
@@ -22,21 +22,34 @@ def main():
 
     print("Knowing how to find limits is quite important in calculus as you can find"
           " where a function is going at a certain point even if the function isn't defined at that point!")
-    print("This program will help you understand how to find limits and their continuity by testing you and providing examples")
-
-    user_input = input("Do you wish to proceed? (This will show you an example. Press y to proceed and n to stop)")
+    print("This program will help you understand how to find limits and their continuity by explaining the concept, testing you and providing examples")
+    user_input = input("Do you wish to proceed? (By pressing y, you will be taught the concept of limits and continuity and how to find them. Pressing n will stop the program)")
     while user_input != 'n':
-
         if user_input == 'y':
-            example_graph()
-            continue
+            print("")
+            break
+
 
         elif user_input == 'n':
             break
+        else:
+            print("Invalid input")
+
+    user_input = input("Do you wish to proceed? (This will show you an example. Press y to proceed and n to stop the program)")
+    while user_input != 'n':
+
+        if user_input == 'y':
+            print("Q1: Find the limit at x = 1")
+            example_graph()
+            print("If you take x = 1 from the right (1.0000000001) and from the left (0.9999999999), you will find that")
+            print("as x approaches x = 1 from the right, y is equal to 2 and as x approaches x = 1 from the left, y is also equal to 2 ")
+            print("Therefore the limit is equal to 2.")
+            break
+
 
         else:
             print("Invalid input")
-    user_input = input("Do you wish to proceed? (This will show you an example. Press y to proceed and n to stop)")
+    user_input = input("Do you wish to proceed? (This will show you another example example. Press y to proceed and n to stop the program)")
 
 
 
