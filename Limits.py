25233ff551c1b041853e6fff274660367be6bee6
SaifW4 <swa1150@rit.edu>
2022-04-07 15:43:37 +0400

    y

--- Limits.py
+++ Limits.py
@@ -38,17 +38,17 @@ def main():
           " where a function is going at a certain point even if the function isn't defined at that point!")
     print("This program will help you understand how to find limits and their continuity by explaining the concept, testing you and providing examples")
     user_input = input("Do you wish to proceed? (By pressing y, you will be taught the concept of limits and continuity and how to find them. Pressing n will take you to the next step)")
+
     while user_input != 'n':
         if user_input == 'y':
             print("")
             break
-
-
-        elif user_input == 'n':
+        elif user_input =='n':
             break
         else:
             print("Invalid input")
-            break
+        user_input = input(
+            "Do you wish to proceed? (By pressing y, you will be taught the concept of limits and continuity and how to find them. Pressing n will take you to the next step)")
 
     user_input = input("Do you wish to proceed? (By pressing y you will be shown an example. Pressing n will take you to the next step)")
     while user_input != 'n':
@@ -61,19 +61,17 @@ def main():
             print("Therefore the limit is equal to 2.")
             print("Since the limit from the right is equal to the limit from the left, there is continuity at this point.")
             break
-        elif user_input == 'n':
+        elif user_input =='n':
             break
-
-
         else:
-            print("Invalid input")
-            break
+            input("Invalid input")
+        user_input = input(
+            "Do you wish to proceed? (By pressing y you will be shown an example. Pressing n will take you to the next step)")
+
     print(" - - - - - - - - - - - - - - - - - - - - - - - - - -")
-    user_input = input("Do you wish to proceed? (Press y to see another example. Pressing n will take you to the next step)")
+    user_input = input("Do you wish to proceed? (Press y to see another example. Pressing anything else will take you to the next step)")
     while user_input != 'n':
-       if user_input == 'n':
-            break
-       elif user_input == 'y':
+       if user_input == 'y':
            print("Q2: Find the limit at x = 2")
            example_graph_2()
            print("If you take x = 2 from the right (2.0000000001) and from the left (1.9999999999), you will find that")
@@ -83,16 +81,19 @@ def main():
            print("Since the limit does not exist, there is no continuity at this point.")
            print("Note that even though the function is not continuous at x = 2, it still has a left-side continuity because f(2) = 1 and the limit from the left is equal to 1")
            break
+       elif user_input == 'n':
+           break
        else:
            print("Invalid input")
+       user_input = input(
+           "Do you wish to proceed? (Press y to see another example. Pressing anything else will take you to the next step)")
     print(" - - - - - - - - - - - - - - - - - - - - - - - - - -")
     print("The answer structure should be as follows: 'Limit answer (Number)', 'Continuity answer (either Yes or No)'")
     print("Please save the graph figure in order to have it always displayed next to the questions.")
     answers = []
     user_input = input("Are you ready to start the quiz? (y if yes and n if no)")
-    for i in range(1,6):
-        time.sleep(1)
-        print(i)
+
+
 
     while user_input != 'n':
         if user_input == 'n':
@@ -104,7 +105,7 @@ def main():
 
             while user_answer1 != 432:
                 if user_answer1 == "1, Yes":
-                    answers.append(user_answer1)
+                    answers.append('Q1: 1, Yes')
                     print("Correct!")
                     break
                 else:
@@ -116,7 +117,7 @@ def main():
 
             while user_answer2 != 432:
                 if user_answer2 == "DNE, DNE":
-                    answers.append(user_answer2)
+                    answers.append("Q2: DNE, DNE")
                     print("Correct!")
                     break
                 else:
@@ -129,7 +130,7 @@ def main():
 
             while user_answer3 != 432:
                 if user_answer3 == "3, Yes":
-                    answers.append(user_answer3)
+                    answers.append("Q3: 3, Yes")
                     print("Correct!")
                     break
                 else:
@@ -141,7 +142,7 @@ def main():
 
             while user_answer4 != 432:
                 if user_answer4 == '2, Yes':
-                    answers.append(user_answer4)
+                    answers.append('Q4: 2, Yes')
                     print("Correct!")
                     break
                 else:
@@ -156,7 +157,7 @@ def main():
 
             while user_answer5 != 432:
                 if user_answer5 == 'DNE, No, LSC':
-                    answers.append(user_answer5)
+                    answers.append('Q5: DNE, No, LSC')
                     print("Correct!")
                     break
                 else:
@@ -164,14 +165,20 @@ def main():
                     break
 
 
+        else:
+            print("Invalid input")
+            user_input = input("Are you ready to start the quiz? (y if yes and n if no)")
+        break
+
+    print("These are the questions that you got right:", answers)
 
 
 
 
 
-            print(answers)
 
-            break
+
+
 
 
 main()
