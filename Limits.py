32e85588872626ba32cbdab0f8a9395b5b3eb90e
SaifW4 <swa1150@rit.edu>
2022-04-07 05:14:15 +0400

    y

--- Limits.py
+++ Limits.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 
 
@@ -22,9 +23,14 @@ def example_graph_2():
     plt.show()
 
 
-#def quiz_graph():
- #   x_axis =
-  #  y_axis =
+def quiz_graph():
+    x_axis = [1,1,3,3,4,5,6,7]
+    y_axis = [0,1,1,3,3,2,0.5,0]
+    plt.title("f(x)")
+    plt.xlabel("x-axis")
+    plt.ylabel("y-axis")
+    plt.plot(x_axis, y_axis, color="red")
+    plt.show()
 
 def main():
 
@@ -42,16 +48,18 @@ def main():
             break
         else:
             print("Invalid input")
+            break
 
     user_input = input("Do you wish to proceed? (By pressing y you will be shown an example. Pressing n will take you to the next step)")
     while user_input != 'n':
 
         if user_input == 'y':
-            print("Q1: Find the limit at x = 1")
+            print("Q1: Find the limit at x = 1 and determine if there is continuity at that point or not:")
             example_graph_1()
             print("If you take x = 1 from the right (1.0000000001) and from the left (0.9999999999), you will find that")
             print("as x approaches x = 1 from the right, y is equal to 2 and as x approaches x = 1 from the left, y is also equal to 2 ")
             print("Therefore the limit is equal to 2.")
+            print("Since the limit from the right is equal to the limit from the left, there is continuity at this point.")
             break
         elif user_input == 'n':
             break
@@ -59,6 +67,8 @@ def main():
 
         else:
             print("Invalid input")
+            break
+    print(" - - - - - - - - - - - - - - - - - - - - - - - - - -")
     user_input = input("Do you wish to proceed? (Press y to see another example. Pressing n will take you to the next step)")
     while user_input != 'n':
        if user_input == 'n':
@@ -68,11 +78,69 @@ def main():
            example_graph_2()
            print("If you take x = 2 from the right (2.0000000001) and from the left (1.9999999999), you will find that")
            print(
-               "as x approaches x = 2 from the right, y is equal to 2 and as x approaches x = 2 from the left, y is also equal to 2 ")
-           print("Therefore the limit is equal to 2.")
+               "as x approaches x = 2 from the right, y is equal to 2 and as x approaches x = 2 from the left, y is equal to 1 ")
+           print("Therefore the limit does not exist.")
+           print("Since the limit does not exist, there is no continuity at this point.")
            break
        else:
            print("Invalid input")
+    print(" - - - - - - - - - - - - - - - - - - - - - - - - - -")
+    print("The answer structure should be as follows: 'Limit answer (Number only)', 'Continuity answer (either Yes or No)'")
+    user_input = input("Are you ready to start the quiz? (y if yes and n if no)")
+    while user_input != 'n':
+        if user_input == 'n':
+            break
+        elif user_input == 'y':
+            print("Answer the following questions based on the following graph. You only have one try. Write DNE of the limit does not exist and/or the continuity does not exist")
+            quiz_graph()
+            user_answer = input("Q1: What is the limit at x = 1.5 and is there continuity at this point?")
+            while user_answer != 432:
+                if user_answer == "1, Yes" or user_answer == "Yes, 1":
+                    print("Correct!")
+                    break
+                else:
+                    print("Incorrect")
+                    break
+            user_answer = input(
+                "Q2: What is the limit at x = 3 and is there continuity at this point?")
+            while user_answer != 432:
+                if user_answer == "DNE, DNE":
+                    print("Correct!")
+                    break
+                else:
+                    print("Incorrect")
+                    break
+            user_answer = input(
+                "Q3: What is the limit at x = 4 and is there continuity at this point?")
+
+
+            while user_answer != 432:
+                if user_answer == "3, Yes":
+                    print("Correct!")
+                    break
+                else:
+                    print("Incorrect")
+                    break
+            user_answer = input(
+                "Q4: What is the limit at x = 5 and is there continuity at this point?")
+            while user_answer != 432:
+                if user_answer == 1:
+                    print("Correct!")
+                    break
+                else:
+                    print("Incorrect")
+                    break
+            user_answer = input(
+                "Q5: What is the limit at x = 7 and is there continuity at this point?")
+            while user_answer != 432:
+                if user_answer == 1:
+                    print("Correct!")
+                    break
+                else:
+                    print("Incorrect")
+                    break
+            break
+
 
 main()
 
